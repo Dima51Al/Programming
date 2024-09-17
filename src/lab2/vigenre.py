168bd4ca5cca2aec9468da949259bbce11133c61
Dima51AL <dmitry290306@gmail.com>
2024-09-17 22:15:03 +0300

    Прошел тесты виженера

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,9 +9,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    for i in range(len(plaintext)):
 
+        time = ord(plaintext[i])
+        if 65 <= time <= 90:
+            shift = ord(keyword[i % len(keyword)]) -65
+            c = ((time - 65) + shift) % 26 + 65
+            ciphertext += chr(c)
+        elif 97 <= time <= 122:
+            shift = ord(keyword[i % len(keyword)]) - 97
+            c = ((time - 97) + shift) % 26 + 97
+            ciphertext += chr(c)
+        else:
+            ciphertext += i
+
+    return ciphertext
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
